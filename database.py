56ba3bd2cb28d4e65a7acc9ef658fc3ae2323b51
ManishGandhiDodda <manishgandhi02@gmail.com>
2026-01-03 22:00:42 +0530

    Added

--- database.py
+++ database.py
@@ -5,20 +5,23 @@ import boto3
 
 load_dotenv()
 
-sb_url = os.getenv("SUPABASE_API_URL")
-sb_key = os.getenv("SUPABASE_SERVICE_KEY")
+# Supabase setup
+supabase_url = os.getenv("SUPABASE_API_URL")
+supabase_key = os.getenv("SUPABASE_SERVICE_KEY")
 
-if not sb_url or not sb_key:
-    raise ValueError("Missing Supabase Details")
+if not supabase_url or not supabase_key:
+    raise ValueError("Missing Supabase credentials in environment variables")
 
-supabase: Client = create_client(supabase_url=sb_url,supabase_key=sb_key)
+supabase: Client = create_client(supabase_url, supabase_key)
+
 
 # S3 Setup
-
-s3_client = boto3.client("s3",
-                         endpoint_url=os.getenv('AWS_ENDPOINT_URL_S3'),
-                         aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-                         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                         region_name=os.getenv('AWS_REGION'))
+s3_client = boto3.client(
+    "s3", 
+    endpoint_url=os.getenv('AWS_ENDPOINT_URL_S3'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
 
 BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
